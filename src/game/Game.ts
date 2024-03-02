9aa4db33ade7a5454aae7bf94452f79182fc7440
Antoine GANNAT <antoine.gannat@gmail.com>
2024-03-02 20:04:11 +0100

    add camera controls

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,48 +1,104 @@
 import { app } from "../Pixi";
 import { Cell } from "./components/Cell";
-import { CELL_SIZE } from "./constants";
+import { CAMERA_MAX_SCALE, CAMERA_MIN_SCALE, CELL_SIZE } from "./constants";
+import { IPosition } from "./types";
+
+const map = [
+  [0, 0, 1, 1, 1, 1, 1],
+  [0, 0, 1, 1, 0, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 0],
+  [0, 0, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+];
 
 class Game {
+  private lastClick: IPosition = { x: -1, y: -1 };
   constructor() {
-    window.addEventListener("keydown", this.onKeyDown.bind(this));
-    app.stage.addChild(new Cell({ x: 200, y: 100 }, "#FF0000").element);
-    app.stage.addChild(
-      new Cell(
-        {
-          x: 200 + CELL_SIZE * 0.88,
-          y: 100 + CELL_SIZE / 2 - CELL_SIZE * 0.05,
-        },
-        "#FF0000"
-      ).element
-    );
+    app.stage.scale.x = app.stage.scale.y = CAMERA_MIN_SCALE * 5;
+    window.addEventListener("mousedown", this.onMouseDown.bind(this));
+    window.addEventListener("mouseup", this.onMouseUp.bind(this));
+    window.addEventListener("mousemove", this.onMouseMove.bind(this));
+    window.addEventListener("wheel", this.onWheel.bind(this));
+
+    map.forEach((elements, row) => {
+      elements.forEach((element, column) => {
+        if (element === 0) {
+          return;
+        }
+        app.stage.addChild(
+          new Cell(
+            this.mapPositionToScreenPosition({ x: column, y: row }),
+            "#FFFFFF"
+          ).element
+        );
+      });
+    });
   }
 
   public destroy() {
-    window.removeEventListener("keydown", this.onKeyDown.bind(this));
+    window.removeEventListener("mousedown", this.onMouseDown.bind(this));
+    window.removeEventListener("mouseup", this.onMouseUp.bind(this));
+    window.removeEventListener("mousemove", this.onMouseMove.bind(this));
+    window.removeEventListener("wheel", this.onWheel.bind(this));
   }
 
-  private onKeyDown(event: KeyboardEvent) {
-    switch (event.key) {
-      case "ArrowUp":
-        app.stage.position.y -= 10;
-        break;
-      case "ArrowDown":
-        app.stage.position.y += 10;
-        break;
-      case "ArrowLeft":
-        app.stage.position.x -= 10;
-        break;
-      case "ArrowRight":
-        app.stage.position.x += 10;
-        break;
-      case "PageUp":
-        app.stage.scale.x += 0.1;
-        app.stage.scale.y += 0.1;
-        break;
-      case "PageDown":
-        app.stage.scale.x -= 0.1;
-        app.stage.scale.y -= 0.1;
-        break;
+  private mapPositionToScreenPosition({ x, y }: IPosition): IPosition {
+    return {
+      x: (x - y) * (CELL_SIZE * 0.88) + y,
+      y: (y + x) * (CELL_SIZE / 2 - CELL_SIZE * 0.05) + y * 8,
+    };
+  }
+
+  private onMouseDown(event: MouseEvent) {
+    this.lastClick = { x: event.clientX, y: event.clientY };
+  }
+
+  private onMouseUp() {
+    this.lastClick = { x: -1, y: -1 };
+  }
+
+  private onMouseMove(event: MouseEvent) {
+    // check which direction the mouse is moving
+    if (this.lastClick.x === -1) {
+      return;
+    }
+
+    const deltaX = Math.abs(event.clientX - this.lastClick.x);
+    const deltaY = Math.abs(event.clientY - this.lastClick.y);
+
+    if (event.clientX > this.lastClick.x) {
+      app.stage.position.x += deltaX;
+    } else if (event.clientX < this.lastClick.x) {
+      app.stage.position.x -= deltaX;
+    }
+
+    if (event.clientY > this.lastClick.y) {
+      app.stage.position.y += deltaY;
+    } else if (event.clientY < this.lastClick.y) {
+      app.stage.position.y -= deltaY;
+    }
+
+    console.log(app.stage.position);
+    this.lastClick = { x: event.clientX, y: event.clientY };
+  }
+
+  private onWheel(event: WheelEvent) {
+    if (event.deltaY > 0) {
+      app.stage.scale.x -= 0.1;
+      app.stage.scale.y -= 0.1;
+    } else {
+      app.stage.scale.x += 0.1;
+      app.stage.scale.y += 0.1;
+    }
+    if (app.stage.scale.x > CAMERA_MAX_SCALE) {
+      app.stage.scale.x = CAMERA_MAX_SCALE;
+      app.stage.scale.y = CAMERA_MAX_SCALE;
+    }
+    if (app.stage.scale.x < CAMERA_MIN_SCALE) {
+      app.stage.scale.x = CAMERA_MIN_SCALE;
+      app.stage.scale.y = CAMERA_MIN_SCALE;
     }
   }
 }
--- src/game/constants.ts
+++ src/game/constants.ts
@@ -1,1 +1,3 @@
 export const CELL_SIZE = 300;
+export const CAMERA_MAX_SCALE = 2;
+export const CAMERA_MIN_SCALE = 0.1;
